754e1fdc3075f5f2cf571103f17d0be7a71cd956
ARUNAVO RAY <rayanup3@gmail.com>
2023-01-07 16:03:26 +0530

    Initial Commit

--- Nothing X MacOS/Nothing_X_MacOSApp.swift
+++ Nothing X MacOS/Nothing_X_MacOSApp.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 @main
 struct Nothing_X_MacOSApp: App {
+    @State var currentNumber: String = "1"
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        
+        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+            // 3
+            Button("One") {
+                currentNumber = "1"
+            }
+            Button("Two") {
+                currentNumber = "2"
+            }
+            Button("Three") {
+                currentNumber = "3"
+            }
         }
     }
 }
