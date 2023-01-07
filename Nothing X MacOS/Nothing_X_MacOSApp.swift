1eaa06fa36fa924a15c9392998fa642a36e635fc
ARUNAVO RAY <rayanup3@gmail.com>
2023-01-07 15:48:41 +0530

    Initial Commit

---
+++ Nothing X MacOS/Nothing_X_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nothing_X_MacOSApp.swift
+//  Nothing X MacOS
+//
+//  Created by Arunavo Ray on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Nothing_X_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nothing X MacOSUITests/Nothing_X_MacOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Nothing_X_MacOSUITestsLaunchTests.swift
+//  Nothing X MacOSUITests
+//
+//  Created by Arunavo Ray on 07/01/23.
+//
+
+import XCTest
+
+final class Nothing_X_MacOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
